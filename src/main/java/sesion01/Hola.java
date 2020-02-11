2de51e1c9533d835be3bda0874e7c169c4255c96
Mario <mogros@hotmail.com>
2020-02-10 21:38:51 -0500

    segundo

--- src/main/java/sesion01/Hola.java
+++ src/main/java/sesion01/Hola.java
@@ -6,7 +6,7 @@ public class Hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mund o");
 		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("cual es tu nnombre");
