f1eb847923c1603f6326226e1db6c588fe14ce11
Mario <mogros@hotmail.com>
2020-02-10 21:14:21 -0500

    Primer commit

---
+++ src/main/java/sesion01/Hola.java
@@ -0,0 +1,21 @@
+package sesion01;
+
+import java.util.Scanner;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("cual es tu nnombre");
+		
+		String nombre=sc.nextLine();
+		System.out.println("Hola "+nombre);
+		
+		sc.close();
+	}
+
+}
+
